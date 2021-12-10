714a9e3253cb779438acdb2d6a2720efde72509a
Tyler <tyler.wong@ontariotechu.net>
2021-12-09 21:18:46 -0500

    Added joystick player movement to player prefab

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    public Joystick moveStick;
+    public Joystick camStick;
+
+    Vector3 direction;
+
+    float jHorizontal = 0f;
+    float jVertical = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    public void MovePlayer()
+    {
+        jHorizontal = moveStick.Horizontal * speed;
+        jVertical = moveStick.Vertical * speed;
+        direction = new Vector3(jHorizontal, 0f, jVertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg; //+cam.eulerAngles.y
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
